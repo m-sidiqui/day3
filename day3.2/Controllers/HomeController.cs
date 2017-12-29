7ef26678dacd05536997467880f9b037b0a51df7
mussaddiqsiddique@yahoo.com <mussaddiqsiddique@yahoo.com>
2017-12-29 13:11:16 +0530

    Add project files.

---
+++ day3.2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace day3._2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string id)
+        {
+            TempData["siddiqui"] = id;
+            return View();
+        }
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
